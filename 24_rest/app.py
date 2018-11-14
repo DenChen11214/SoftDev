81c6e7c89fb4426c5d6aef489ca1ef099dd51740
Johnson Li <jli39@stuy.edu>
2018-11-13 21:01:59 -0500

    done

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -5,17 +5,11 @@ import json
 app = Flask(__name__)
 
 @app.route("/")
-def api():
-    html = urlopen("https://api.nasa.gov/planetary/apod?api_key=BuBI98q6zPb0F9DTHqAkZXlSEWAKJ5A8hTTAV8qP")
-    # print(html)
-    text = json.loads(html.read())
-    # print(text)
-    return render_template("template.html", title=text["title"], media=text["url"])
+def nasa():
+    res = urlopen("https://api.nasa.gov/planetary/apod?api_key=BuBI98q6zPb0F9DTHqAkZXlSEWAKJ5A8hTTAV8qP")
+    info = json.loads(res.read())
+    return render_template("template.html", title=info["title"], media=info["url"], explanation=info["explanation"], media_type=info["media_type"])
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
-
-
-# https://api.nasa.gov/planetary/apod?api_key=BuBI98q6zPb0F9DTHqAkZXlSEWAKJ5A8hTTAV8qP
