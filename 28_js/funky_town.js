4ebd2cf6c365691e788bdc3900d936e1ea566307
Johnson Li <jli39@stuy.edu>
2018-12-18 14:00:08 -0500

    removed some semicolons

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -5,7 +5,7 @@
 
 var fibonacci = (n) => {
     return fibHelp(1, 0, n);
-};
+}
 
 // Helper for fibonacci
 var fibHelp = (startNum, sumSoFar, numTimes) => {
@@ -13,17 +13,17 @@ var fibHelp = (startNum, sumSoFar, numTimes) => {
         return sumSoFar;
     }
     return fibHelp(startNum + sumSoFar, startNum, numTimes - 1);
-};
+}
 
 var gcd = (a, b) => {
     if(b == 0){
         return a;
     }
     return gcd(b, a % b);
-};
+}
 
 var students = ["Bob", "Tim", "Kevin", "John", "Sally"];
 var randomStudent = () => {
     var randomIndex = Math.floor(Math.random() * students.length);
     return students[randomIndex];
-};
+}
